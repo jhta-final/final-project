aaadeeb63bbcb05b48f7de9361a0c051dd9a309e
JangJuRi <wkdwnfl119@naver.com>
2020-08-12 09:20:08 +0900

    jang moims.xml 7

--- somoim/src/main/java/com/sample/dao/MoimDao.java
+++ somoim/src/main/java/com/sample/dao/MoimDao.java
@@ -9,11 +9,11 @@ public interface MoimDao {
 
 	void insertMoim(MoimMain moimMain);
 	List<MoimMain> getAllMoims();
-	MoimMain getMoimsByNo(long moimNo);
+	MoimMain getMoimByNo(long moimNo);
 	void updateMoim(MoimMain moimMain);
 	void deleteMoim(long moimNo);
 	
 	void insertJoinUser(MoimJoinUser moimJoinUser);
-	List<MoimJoinUser> getAllJoinUserByMoimNo(long moimNo);
+	List<MoimJoinUser> getAllJoinUsersByMoimNo(long moimNo);
 	void deleteJoinUser(MoimJoinUser moimJoinUser);
 }
