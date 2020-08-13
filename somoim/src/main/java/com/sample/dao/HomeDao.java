6b6666a5e06e77fe699f49e7b27212bec10caaf6
Jung-HyunJo <69337603+Jung-HyunJo@users.noreply.github.com>
2020-08-13 17:40:00 +0900

    test4

--- somoim/src/main/java/com/sample/dao/HomeDao.java
+++ somoim/src/main/java/com/sample/dao/HomeDao.java
@@ -6,6 +6,9 @@ import com.sample.dto.MoimMainDto;
 
 public interface HomeDao {
 
+	// 랜덤 모임 표시하기
 	List<MoimMainDto> randomMoims();
+	
+	// 지역별 선호 모임 랜덤 표시
 	List<MoimMainDto> locationMoims(long locationNo);
 }
