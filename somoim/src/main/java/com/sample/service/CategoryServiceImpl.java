01ef3c01f9c8c1b200ad6a3300b880cebb031df8
JangJuRi <wkdwnfl119@naver.com>
2020-08-14 16:10:27 +0900

    jang category

---
+++ somoim/src/main/java/com/sample/service/CategoryServiceImpl.java
@@ -0,0 +1,49 @@
+package com.sample.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sample.dao.CategoryDao;
+import com.sample.dto.MoimMainDto;
+import com.sample.vo.MoimDates;
+
+@Service
+@Transactional
+public class CategoryServiceImpl implements CategoryService {
+
+	@Autowired CategoryDao categoryDao;
+	
+	// 메인카테고리별 모임 조회
+	@Override
+	public List<MoimMainDto> getMoimsByMainCategory(long mainCateNo) {
+		return categoryDao.selectMoimsByMainCategory(mainCateNo);
+	}
+
+	// 서브카테고리별 모임 조회
+	@Override
+	public List<MoimMainDto> getMoimsBySubCategory(long subCateNo) {
+		return categoryDao.selectMoimsBySubCategory(subCateNo);
+	}
+
+	// 지역별 모임 조회
+	@Override
+	public List<MoimMainDto> getMoimsByLocation(long locationNo) {
+		return categoryDao.selectMoimsByLocation(locationNo);
+	}
+
+	// 날짜별 모임 조회
+	@Override
+	public List<MoimMainDto> getMoimsByDate(MoimDates moimDates) {
+		return categoryDao.selectMoimsByDate(moimDates);
+	}
+
+	// 좋아요순 모임 조회
+	@Override
+	public List<MoimMainDto> getMoimsByLikes() {
+		return categoryDao.selectMoimsByLikes();
+	}
+
+}
---
+++ somoim/src/main/java/com/sample/vo/MoimDates.java
@@ -0,0 +1,36 @@
+package com.sample.vo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MoimDates {
+
+	private Date startDate;
+	private Date endDate;
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String stringStartDate) {
+		stringStartDate = stringStartDate.replace("T", " ");
+		try {
+			this.startDate = sdf.parse(stringStartDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String stringEndDate) {
+		stringEndDate = stringEndDate.replace("T", " ");
+		try {
+			this.endDate = sdf.parse(stringEndDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+}
