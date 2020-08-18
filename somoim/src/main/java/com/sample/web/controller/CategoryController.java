3ed3ecf7adafdbce0e096f6ca8dade3ba338f22a
JangJuRi <wkdwnfl119@naver.com>
2020-08-18 10:46:28 +0900

    jang test

--- somoim/src/main/java/com/sample/web/controller/CategoryController.java
+++ somoim/src/main/java/com/sample/web/controller/CategoryController.java
@@ -1,10 +1,14 @@
 package com.sample.web.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sample.dto.MoimMainDto;
 import com.sample.service.CategoryService;
 
 @Controller
@@ -14,4 +18,14 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 	
+	@GetMapping("/test.do")
+	public String test() {
+		
+		List<MoimMainDto> list = categoryService.getMoimsByMainCategory(1);
+		for(MoimMainDto m : list) {
+			System.out.println(m.getFriends());
+		}
+		
+		return "";
+	}
 }
