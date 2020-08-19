66aea726671fa22350a4ff880f479d20a528f261
Jung-HyunJo <69337603+Jung-HyunJo@users.noreply.github.com>
2020-08-19 14:33:21 +0900

    test5

--- somoim/src/main/java/com/sample/vo/MoimFavoriteMoim.java
+++ somoim/src/main/java/com/sample/vo/MoimFavoriteMoim.java
@@ -6,10 +6,27 @@ public class MoimFavoriteMoim {
 
 	private long moimNo;
 	private String userId;
+	private String title;
+	private String content;
 	private Date likedDate;
 	
 	
-	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
 	public Date getLikedDate() {
 		return likedDate;
 	}
