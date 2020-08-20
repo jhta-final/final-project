c30b753bee74b69724351c1d2dc1ae1cd0b23274
kwsoj123@gmail.com <kwsoj123@naver.com>
2020-08-20 09:49:17 +0900

    kim - 마이페이지 수정 1

--- somoim/src/main/java/com/sample/web/controller/MypageController.java
+++ somoim/src/main/java/com/sample/web/controller/MypageController.java
@@ -1,9 +1,14 @@
 package com.sample.web.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sample.dto.MoimFollowDto;
 import com.sample.dto.MoimJoinUserMoimDto;
@@ -20,6 +26,7 @@ import com.sample.service.UserService;
 import com.sample.vo.MoimBoard;
 import com.sample.vo.MoimPhoto;
 import com.sample.vo.MoimUser;
+import com.sample.web.form.ModifyForm;
 
 @Controller
 @RequestMapping("/mypage")
@@ -69,8 +76,28 @@ public class MypageController {
 	
 	// 내정보수정
 	@PostMapping("/modify.do")
-	public String modifyUser (@ModelAttribute("user") MoimUser user) {
-		userService.modifyUser(user);
+	public String modifyUser (@ModelAttribute("modifyform") ModifyForm modifyForm) {
+		MoimUser modifyUser = new MoimUser();
+		modifyUser.setId(user.getId());
+		BeanUtils.copyProperties(modifyForm, modifyUser);
+		
+		MultipartFile upfile = modifyForm.getUpfile();
+		String filename = upfile.getOriginalFilename();
+			
+		File file = new File("C:\\final_project\\workspace\\somoim\\src\\main\\webapp\\resources\\profileImage"+filename);
+		FileOutputStream fos;
+		try {
+			file.createNewFile();
+			fos = new FileOutputStream(file);
+			fos.write(upfile.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		modifyUser.setProfileImage(filename);
+		userService.modifyUser(modifyUser);
 		return "mypage/mypage.tiles?modify=success";
 	}
 }
